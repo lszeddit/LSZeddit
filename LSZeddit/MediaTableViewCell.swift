67f54354089afd0f9c7be4ee9c3470b913f59c25
Carlos Acosta <carlos@lsztech.com>
2019-04-10 14:17:35 -0400

    Slight refactor of MediaTVC

--- LSZeddit/MediaTableViewCell.swift
+++ LSZeddit/MediaTableViewCell.swift
@@ -1,11 +1,3 @@
-//
-//  MediaTableViewCell.swift
-//  LSZeddit
-//
-//  Created by Carlos Acosta on 4/9/19.
-//  Copyright © 2019 LSZTech. All rights reserved.
-//
-
 import UIKit
 
 class MediaTableViewCell: UITableViewCell {
@@ -23,13 +15,10 @@ class MediaTableViewCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 
 }
