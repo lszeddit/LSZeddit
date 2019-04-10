337bc275cf13a88da4570a893992da823f9d505a
Carlos Acosta <carlos@lsztech.com>
2019-04-10 14:17:09 -0400

    Slight refactor of Strings file

--- LSZeddit/Strings.swift
+++ LSZeddit/Strings.swift
@@ -1,11 +1,3 @@
-//
-//  Strings.swift
-//  LSZeddit
-//
-//  Created by Carlos Acosta on 4/10/19.
-//  Copyright © 2019 LSZTech. All rights reserved.
-//
-
 import Foundation
 
 enum Strings: String{
