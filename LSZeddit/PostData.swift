ccae16a5a529243bfd17cdfaa5a373cd09f85bca
Carlos Acosta <carlos@lsztech.com>
2019-04-14 22:12:32 -0400

    Refactored class

---
+++ LSZeddit/PostData.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+
+struct PostData: Codable{
+    
+    var title: String
+    var thumbnail: String?
+    var author: String
+    var created: Double
+    var commentCount: Int
+    var score: Int
+    var url: String
+    var postHint: String?
+    var selfText: String?
+    var isSelf: Bool
+    
+    private enum CodingKeys: String, CodingKey{
+        case title
+        case thumbnail
+        case author
+        case created
+        case commentCount = "num_comments"
+        case score
+        case url
+        case postHint = "post_hint"
+        case selfText = "selftext"
+        case isSelf = "is_self"
+    }
+}
