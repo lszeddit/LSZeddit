a5e128a8c09e11dca36084b10257d82478a15fba
Carlos Acosta <carlos@lsztech.com>
2019-04-14 22:16:05 -0400

    Refactored class

--- LSZeddit/RedditAPIManager.swift
+++ LSZeddit/RedditAPIManager.swift
@@ -1,27 +1,12 @@
-//
-//  RedditAPIManager.swift
-//  LSZeddit
-//
-//  Created by Carlos Acosta on 4/9/19.
-//  Copyright © 2019 LSZTech. All rights reserved.
-//
-
 import Foundation
-
 
 class RedditAPIManager {
     
-    let baseURL = "https:reddit.com/top.json"
-    
+    static let baseURL = "https:reddit.com/top.json"
     static let shared = RedditAPIManager()
     static let getNewEndpoint = "?sort=new"
     
-    
-    
     func getTopPosts(onComplete: @escaping(Any?) -> Void){
-        let stringURL : String = baseURL
-        
-        
         guard let url = URL(string: "https://www.reddit.com/top.json?limit=50") else {return}
         
         URLSession.shared.dataTask(with: url) { (data, response, error) in
@@ -41,42 +26,7 @@ class RedditAPIManager {
         }.resume()
     }
 }
-struct RedditResponse: Codable{
-    var kind: String
-    var data: RedditResponseData
-}
 
-struct RedditResponseData: Codable{
-    var children: [Post]
-}
 
-struct Post: Codable{
-    var data: PostData
-}
 
-struct PostData: Codable{
-    
-    var title: String
-    var thumbnail: String?
-    var author: String
-    var created: Double
-    var commentCount: Int
-    var score: Int
-    var url: String
-    var postHint: String?
-    var selfText: String?
-    var isSelf: Bool
-    
-    private enum CodingKeys: String, CodingKey{
-        case title
-        case thumbnail
-        case author 
-        case created
-        case commentCount = "num_comments"
-        case score
-        case url
-        case postHint = "post_hint"
-        case selfText = "selftext"
-        case isSelf = "is_self"
-    }
-}
+
